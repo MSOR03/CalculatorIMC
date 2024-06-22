fec6a2f7ad2d00cd5cb122bfd4f8e8976b6fcc59
MSOR03 <molartera@unal.edu.co>
2024-06-22 01:05:52 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+function calculateIMC(){
+    weight=parseFloat(document.getElementById('weight').value);
+    height=parseFloat(document.getElementById('height').value)/100;
+
+    if(isNaN(height) || isNaN(weight) || height<0 || weight<0){
+        alert('Insert valid number please');
+        return;
+    }
+    
+    let imc= weight/(Math.pow(height,2));
+    alert('Your IMC is '+imc.toFixed(2));
+
+}
+
